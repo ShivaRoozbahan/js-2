fce4631fb8cf9845ca40795bd720faaed2d84f60
Shiva <shivaroozbahani@gmail.com>
2024-09-11 17:55:59 +0330

    edit code

--- BMI/script.js
+++ BMI/script.js
@@ -6,9 +6,9 @@ let BMI = (height * height) / weight;
 
 if (BMI < 18.5) {
   console.log(`BMI: ${BMI} = underweight`);
-} else if (18.5 <= BMI < 25) {
+} else if (18.5 <= BMI && BMI < 25) {
   console.log(`BMI: ${BMI} = normal weight`);
-} else if (25 <= BMI < 30) {
+} else if (25 <= BMI && BMI < 30) {
   console.log(`BMI: ${BMI} = overweight`);
 } else if (BMI >= 30) {
   console.log(`BMI: ${BMI} = obese`);
--- grade letters/script.js
+++ grade letters/script.js
@@ -1,5 +1,17 @@
-const score = 100;
+const score = 60;
 
-if (100 >= score >= 90) {
-  console.log();
+if (90 <= score && score <= 100) {
+  console.log(`${score}: +A`);
+} else if (80 <= score && score <= 89) {
+  console.log(`${score}: A`);
+} else if (70 <= score && score <= 79) {
+  console.log(`${score}: +B`);
+} else if (60 <= score && score <= 69) {
+  console.log(`${score}: B`);
+} else if (50 <= score && score <= 59) {
+  console.log(`${score}: C`);
+} else if (50 > score) {
+  console.log(`${score}: D`);
+} else {
+  console.log("plz nter a positive number!!");
 }
